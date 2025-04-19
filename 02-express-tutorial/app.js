7dc7161f716b61be6ffafead84874cd59821e924
Hugo  Calderon Delgado <hugocalderon@Hugos-MacBook-Pro.local>
2025-04-19 04:49:26 -0700

    week 4

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,10 +1,30 @@
 const express = require('express');
-const { products } = require('./data');
+const { products, people } = require('./data');
+const peopleRouter = require('./routes/people.js');
 const app = express();
 const port = 3000;
 
+//Middleware function
+const logger = (req, res, next) => {
+    const method = req.method;
+    const url = req.url;
+    const time = new Date().toDateString()
+    console.log("Method: ", method);
+    console.log('Url: ', url);
+    console.log("Time: ", time);
+    next()
+}
+
 //Middleware
-app.use(express.static('./public'))
+app.use(express.static('./public'), logger);
+
+
+//parse form data 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+//people router
+app.use("/api/v1/people", peopleRouter);
+
 
 app.get('/api/v1/test', (req, res) => {
     res.json({ message: "It worked!" });
@@ -14,6 +34,11 @@ app.get('/api/v1/test', (req, res) => {
 app.get('/api/v1/products', (req, res) => {
     res.json(products);
 })
+
+//GET people
+// app.get('/api/v1/people', (req, res) => {
+//     res.json(people);
+// })
 
 //GET Product ID
 app.get('/api/v1/products/:productID', (req, res) => {
@@ -63,7 +88,13 @@ app.get('/api/v1/query', (req, res) => {
     res.json(filteredProducts);
 });
 
-//app.post()
+// app.post('api/v1/people', (req, res) => {
+//     if (req.body.name === null){
+//         res.status(400).json({ success: false, message: "Please provide a name" });
+//     }
+//     people.push({ id: people.length + 1, name: req.body.name });
+//     res.status(201).json({ success: true, name: req.body.name });
+// })
 
 app.all('./public', (req, res, next) => {
     next();
